da313745c2bd02905eadc50e5890028b59c091a9
Nando Junior <nando.devs@gmail.com>
2021-06-11 20:53:50 -0300

    Correção de Bug

--- src/main/java/com/nandodevs/apilog/controller/StockController.java
+++ src/main/java/com/nandodevs/apilog/controller/StockController.java
@@ -1,9 +1,16 @@
 package com.nandodevs.apilog.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 import com.nandodevs.apilog.model.dto.StockDTO;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
     
     //Primeiro EndPoint
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
-        return null;
+        return ResponseEntity.ok(dto);
 
     }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+
+        List<StockDTO> lista = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Havan");
+        dto.setPrice(100D);
+        dto.setVariation(10D);
+        dto.setDate(LocalDate.now());
+        lista.add(dto);
+        return ResponseEntity.ok(lista);
+    }
+
+    //Retorna uma só ação
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<StockDTO> findById(@PathVariable Long id){
+        return null;
+    }
+
 }
